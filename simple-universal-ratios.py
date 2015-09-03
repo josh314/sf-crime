166d303150dac713b00b2d8609de530f6441c19c
Joshua Davis <josh314@gmail.com>
2015-09-03 16:19:45 -0400

    Added benchmark script. It works but output CSV is honking huge

---
+++ simple-universal-ratios.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import numpy as np
+
+#Hardcoded file locations
+train_file = 'data/raw/train.csv'
+test_file = 'data/raw/test.csv'
+submission_file = 'simple-universal-ratios-submission.csv'
+
+#load training file to data frame
+train = pd.read_csv(train_file,header=0)
+
+#Aggregate total number of each type of crime in training set
+crime_numbers = train.groupby('Category').size()
+#Create a row of overall probabilities out of the crime numbers
+#This vector is thus normalized to sum up to 1.
+crime_ratios = crime_numbers / len(train)
+#Convert to list
+probs = crime_ratios.values.tolist()
+
+#load test file to data frame
+test = pd.read_csv(test_file,header=0)
+
+#Create a matrix of probabilities for each row of test data
+#Each row gets the same values -- the overall probs.
+probs_array = np.array([probs]*len(test))
+
+#Create empty data frame for submission file
+columns = crime_ratios.index.tolist()
+df = pd.DataFrame(probs_array, columns=columns)
+
+df.insert(loc=0,column='Id',value=test['Id'])
+
+df.to_csv(submission_file,index=False)
+
+
